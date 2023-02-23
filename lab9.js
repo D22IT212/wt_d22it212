ad2aae497d58e0c1e202e0ccd24b58393aea8206
D22IT212 <117340825+D22IT212@users.noreply.github.com>
2023-02-23 11:12:37 +0530

    Add files via upload

---
+++ lab9.js
@@ -0,0 +1,17 @@
+let StudentArr =[
+    {
+        name: "prachi",
+        age: "19",
+    },
+    {
+        name: "hitanshi",
+        age: "20",
+    },
+    {
+        name: "vidhi",
+        age: "18",
+    },  
+]
+StudentArr.forEach (function (s,i) {
+console.log(`Name is ${s} and ID $(i)`)
+});
